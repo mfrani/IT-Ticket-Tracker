a34ff1c8a9f541f72d4b6892a708836373ac9368
mfrani <matt.frani@gmail.com>
2021-05-14 14:51:12 -0700

    adding status dropdown

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function addTicket(e){
     let selectedSeverity = severityInput.options[severityInput.selectedIndex].text;
     let assignmentInput = document.getElementById('assignment');
     let selectedAssignment = assignmentInput.options[assignmentInput.selectedIndex].text;
+    let status = 'OPEN';
 
     //basic form validation 
     if (descriptionInput == ''){
@@ -30,11 +31,12 @@ function addTicket(e){
         return false;
     }
         let ticket ={
+            status:status,
             description: descriptionInput,
             severity: selectedSeverity,
             assignment: selectedAssignment
         }
-    
+        
         tickets.push(ticket);
         console.warn('added', {tickets});
         document.querySelector('form').reset();
@@ -46,28 +48,41 @@ function addTicket(e){
         // localStorage.setItem('MyTicketList', JSON.stringify(tickets));
         storeTicketInput(tickets,'ticketList');
         // let data = JSON.parse(localStorage.getItem("MyTicketList"));
-        renderHtml(descriptionInput, selectedSeverity, selectedAssignment, ticket);
+        renderHtml(status, descriptionInput, selectedSeverity, selectedAssignment, ticket);
  
 }
 
-function renderHtml(descVal, sevVal, assVal, currTicket){
+function renderHtml(status, descVal, sevVal, assVal, currTicket){
     let addTr = document.createElement('tr');
     let addTh = document.createElement('th');
     let addTd1 = document.createElement('td');
     let addTd2 = document.createElement('td');
     let addTd3 = document.createElement('td');
     let addTd4 = document.createElement('td');
+    let addTd5 = document.createElement('td');
     let tableBody = document.getElementById('tableBody');
     let descriptionInput = document.createTextNode(descVal);
     let selectedSeverity = document.createTextNode(sevVal);
     let selectedAssignment = document.createTextNode(assVal);
     let deleteBtn = document.createElement('button');
 
+    let statusDropdown = document.createElement('select');
+    let statusOption1 = document.createElement('option');
+    let statusOption2 = document.createElement('option');
+
+    statusDropdown.setAttribute('id', 'currStatus');
     deleteBtn.setAttribute('type', 'button');
     deleteBtn.classList.add('btn-danger','btn');
-
     deleteBtn.textContent ='Delete';
-    let ticketTable = document.getElementById('ticketTable');
+    
+    statusOption1.setAttribute('value', status);
+    statusOption1.textContent = status;
+    statusDropdown.appendChild(statusOption1);
+
+    statusOption2.setAttribute('value','closed');
+    statusOption2.textContent = 'CLOSED';
+    statusDropdown.appendChild(statusOption2);
+
 
    
     //append table row to table body
@@ -79,16 +94,18 @@ function renderHtml(descVal, sevVal, assVal, currTicket){
     //add scope='row' attribute to tablehead
     addTh.setAttribute('row', 'scope');
 
-    //append td1, td2, td3 to table row
-    addTr.appendChild(addTd1);
+    //append td1, td2, td3, td4 ,td5 to table row
+    addTh.appendChild(addTd1);
     addTr.appendChild(addTd2);
     addTr.appendChild(addTd3);
     addTr.appendChild(addTd4);
+    addTr.appendChild(addTd5);
     //append values to td's
-    addTd1.appendChild(selectedSeverity);
-    addTd2.appendChild(descriptionInput);
-    addTd3.appendChild(selectedAssignment);
-    addTd4.appendChild(deleteBtn);
+    addTd1.appendChild(statusDropdown);
+    addTd2.appendChild(selectedSeverity);
+    addTd3.appendChild(descriptionInput);
+    addTd4.appendChild(selectedAssignment);
+    addTd5.appendChild(deleteBtn);
 
     deleteBtn.addEventListener('click',function(){
         console.warn(currTicket);
@@ -96,7 +113,14 @@ function renderHtml(descVal, sevVal, assVal, currTicket){
         tickets.splice(index, 1);
         storeTicketInput(tickets, 'ticketList');
         tableBody.removeChild(addTr);
+        alert('Ticket deleted!');
     })
+    console.log(currTicket);
+    statusDropdown.addEventListener('change', function(){
+        
+    })
+    
+    
 }
 
 
@@ -117,8 +141,21 @@ function render(data) {
     data.forEach(function(current, index, array) {
         renderHtml(current);
     });
-
 }
 
 
+//modify localStorage
+function setObject(key, obj) {
+    localStorage.setItem(key, JSON.stringify(obj));
+}
 
+function updateItem(key, property, value)
+{
+    var obj = getObject(key);
+    obj[property] = value;    
+    setObject(key, obj);
+}
+
+function getObject(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
